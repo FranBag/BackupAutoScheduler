2422e4a691f85a606d36e10e74a2ab548946ff89
FranBag <fbagneres@yahoo.com>
2025-06-13 13:01:50 -0300

    Añadido verificador de base de datos existente

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import os
+
 from src.models.dbInicializer import createDB
 from src.models.deviceModel import iniciardevice
 from src.models.backupModel import iniciarbackup
+from src.view import GUI
+
+def backup_exist():
+    route = os.path.join(".", "backups.db")
+    return os.path.isfile(route)
 
 if __name__ == "__main__":
-    createDB()
-    iniciardevice()
-    iniciarbackup()
+    if not backup_exist():
+        createDB()
+        iniciardevice()
+        iniciarbackup()
+        print("Se ha creado la base de datos")
+    
+    root = GUI.tk.Tk()
+    app = GUI.BackupGUI(root)
+    root.mainloop()
